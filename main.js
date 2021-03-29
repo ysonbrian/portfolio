96000fab007749c10ea67e38651072526f6632cc
ysonbrian <znlsakxls@gmail.com>
2021-03-29 16:35:41 +0900

    JS: Navbar menu click

--- main.js
+++ main.js
@@ -10,3 +10,15 @@ document.addEventListener('scroll', () => {
     navbar.classList.remove('navbar--dark');
   }
 });
+
+// Handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (e) => {
+  const target = event.target;
+  const link = target.dataset.link;
+  if (link == null) {
+    return;
+  }
+  const scrollTo = document.querySelector(link);
+  scrollTo.scrollIntoView({ behavior: 'smooth' });
+});
